16338adfeef56f3ec6f633e31d4ea1cf3a5196a1
a289459798 <289459798@qq.com>
2013-11-08 06:42:53 +0800

    将每个单词换行打印

--- The-C-Programming-Language-2/1/1-12.c
+++ The-C-Programming-Language-2/1/1-12.c
@@ -1,6 +1,6 @@
 /*
  * 问题：将单词一行一个输出
- * 这里不考虑是否符合单词的规则，将所有空格、制表、换行分开的我们都视为一个单词，就算它是数字
+ * 这里不考虑是否符合单词的规则，将所有空格、制表、换行分开的我们都视为一个单词，尽管它是数字
  */
 #include <stdio.h>
 #define IN  1
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	char c, state = OUT;
 	while((c = getchar()) != EOF){
 		if(c == ' ' || c == '\n' || c == '\t'){
-			if(state == IN){
+			if(state == IN){		/*在单词内与单词外主要用于多个连续空格或制表、换行符的判断*/
 				printf("\n");
 				state = OUT;
 			}

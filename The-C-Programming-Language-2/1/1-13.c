3b32f85c7585b6e07c881f6b80c267528d3c1fef
a289459798 <289459798@qq.com>
2013-11-08 07:44:02 +0800

    打印输入的字符串中单词长度的横向直方图

--- The-C-Programming-Language-2/1/1-13.c
+++ The-C-Programming-Language-2/1/1-13.c
@@ -9,23 +9,23 @@
 #define IN 1
 int main(int argc, char *argv[])
 {
-	int i, j, arr[MAXLENGTH],							//定义数组并申请空间，用于不同长度的单词计数器
-		state = OUT,									//跟教程一样，用于判断此时是在单词里面还是外面
-		count = 0;										//用于对每个单词的计数
+	int i, j, arr[MAXLENGTH],	//定义数组并申请空间，用于不同长度的单词计数器
+		state = OUT,			//跟教程一样，用于判断此时是在单词里面还是外面
+		count = 0;				//用于对每个单词的计数
 	char c,
-		map = '#';										//用于显示直方图
+		map = '#';				//用于显示直方图
 	for(i=0; i<MAXLENGTH; i++){
-		arr[i] = 0;										//将数组的所有元素初始化
+		arr[i] = 0;				//将数组的所有元素初始化
 	}
 
 	while((c = getchar()) != EOF){
-		if(c == ' ' || c == '\t' || c == '\n'){			//此时表示前一个单词的长度计算完成
+		if(c == ' ' || c == '\t' || c == '\n'){	//此时表示前一个单词的长度计算完成
 			state = OUT;
-			if(count >= MAXLENGTH)						//对于大于指定长度的单词通通记录到允许的最大长度计数中
+			if(count >= MAXLENGTH)				//对于大于指定长度的单词通通记录到允许的最大长度计数中
 				arr[MAXLENGTH-1]++;
 			else if(count > 0)
 				arr[count]++;
-			count = 0;									//对单词计数器清零
+			count = 0;							//对单词计数器清零
 		}
 		else if(state == OUT){
 			state = IN;

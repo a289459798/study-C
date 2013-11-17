2cc484b28bdfbce7314357fcc48418eb5c56370f
a289459798 <289459798@qq.com>
2013-11-17 12:47:02 +0800

    用x&(x-1)的方法改写bitcount函数，使其执行效率更高

---
+++ The-C-Programming-Language-2/2/2-9.c
@@ -0,0 +1,22 @@
+/*
+ * 问题：x & (x-1) 用于把最后的1去掉，分析一下原因。并用该方法改写bitcount函数，使执行效率更高
+ *
+ * 原因分析：在求'与'的时候，相同位真与真才只真，当一个数-1后，与他最后一个1对应的数必定变成0，所以在'与'运算的时候，就变成了0
+ */
+#include <stdio.h>
+int main(void)
+{
+	int x = 67;
+	printf("%d\n", bitcount(x));
+	return 0;
+}
+
+int bitcount(unsigned x)
+{
+	int count = 0;
+	while(x != 0){
+		x = x & (x-1);
+		count++;
+	}
+	return count;
+}
